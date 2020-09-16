58cd1d5cd026a1ad8894f57ca5fdc512ae236d2e
can kurt <can.kurt.aa@gmail.com>
2020-09-16 04:33:28 +0300

    downloading part changed because youtube_dl cant give me the path and windows cant save all youtube video titles because bill gates hates me I guess

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -4,27 +4,43 @@ import logging
 import os
 
 
+
+
 class youtube_downloader():
     """uses youtube_dl to download
     https://github.com/ytdl-org/youtube-dl
     """
 
-    def __init__(self, temp_folder_path = "tmp", log_file=None, log_level=20):
-        
-        # downloader variables
-        self.__youtube_dl_options()
-        self.temp_folder_path = temp_folder_path
+    def __init__(self, 
+    logger_name="telegram", 
+    log_file=None, 
+    log_level=20, 
+    temp_folder_path="tmp", 
+    temp_file_name="temp", 
+    bad_chars={'"':'\'','<':'','>':'',':':'','/':'','\\':'','|':'','?':'','*':''},
+    video_formats={"1080p":"137+bestaudio/best", "720p":"136+bestaudio/best", "480p":"135+bestaudio/best", "360p":"134+bestaudio/best", "240p":"133+bestaudio/best"},
+    preferred_video_format="mp4",
+    preferred_audio_codec="mp3"
+    ):
 
-        self.possible_extensions = ['.mp4', '.mkv', '.webm', ".mp3"]
+        # downloader variables
+        self.temp_folder_path = temp_folder_path
+        self.temp_file_name = temp_file_name
+        self.video_formats = video_formats
+        self.bad_chars = bad_chars
+        self.preferred_video_format = preferred_video_format
+        self.preferred_audio_codec = preferred_audio_codec
+        self.__youtube_dl_options()
 
         # logging variables
-        self.logger_name = __name__
+        self.logger_name = logger_name
         self.log_file = log_file
         self.log_level = log_level
         self.__set_logger()
 
     # logging stuff
     def __set_logger(self):
+        """creates the downloader logger"""
         self.logger = logging.getLogger(self.logger_name)  
 
         if(not self.logger.handlers):
@@ -46,65 +62,88 @@ class youtube_downloader():
             
 
     def  __youtube_dl_options(self):
+        """youtube_dl download templates"""
         self.ydl_opts_audio = {
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
+            'preferredcodec': self.preferred_audio_codec,
             'preferredquality': '192',
         }],
         "ffmpeg_location" : "ffmpeg/",
         'format': 'bestaudio/best',
-        # "verbose": True, 
         'noplaylist' : True,
-        "outtmpl": "tmp" + "/%(title)s.%(ext)s",
+        "outtmpl": os.path.join(self.temp_folder_path, self.temp_file_name + ".%(ext)s"),
         # "progress_hooks": [self.__hook],
         }
 
         self.ydl_opts_video = {
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': self.preferred_video_format,
+        }],
         "ffmpeg_location" : "ffmpeg/",
         'format': 'bestvideo+bestaudio',
-        # "verbose": True,
         # "yes_overwrites": True, # not working
         'noplaylist' : True,
-        "outtmpl": "tmp" + "/%(title)s.%(ext)s",
+        "outtmpl": os.path.join(self.temp_folder_path, self.temp_file_name + ".%(ext)s"),
         # "progress_hooks": [self.__hook],
         }
 
-        self.video_formats = {"1080p":"137+bestaudio/best", "720p":"136+bestaudio/best", "480p":"135+bestaudio/best", "360p":"134+bestaudio/best", "240p":"133+bestaudio/best"}
+    def __clear_temp_folder(self):
+        """clears temp folder before download because youtube_dl can't override files and can't give you the path.
+        I need to list all the temp folder to find downloaded file, there can't be another file here or exception occurres"""
+        files = os.listdir(self.temp_folder_path)
+        for f in files:
+            os.remove(os.path.join(self.temp_folder_path, f))
+        
+    def __clean_bad_chars(self, filename):
+        """cleans bad chars from the path because Bill Gates just wants to give me pain"""
+        for char in self.bad_chars:
+            filename = filename.replace(char, self.bad_chars[char])
+        return filename
+
+    def __rename_downloaded_file(self, video_title):
+        """renames downloaded temp file to videos title. 
+        there is no way to know the extension of the file that youtube_dl downloaded so I get all files in the temp folder and rename after download"""
+        temp_file_locations = os.listdir(self.temp_folder_path)
+
+        if(len(temp_file_locations) != 1):
+            self.logger.error("multiple files exists in the temp file")
+            return 0, "downloaded video caused error (possibly because of the video format)"
+        temp_file_location = os.path.join(self.temp_folder_path, temp_file_locations[0])
+
+        if(not os.path.isfile(temp_file_location)):
+            self.logger.error("downloaded file is not a path")
+            return 0, "download error"
+
+        try:
+            # fix bad chars for windows
+            video_title = self.__clean_bad_chars(video_title)
+
+            _, file_extension = os.path.splitext(temp_file_location)
+            new_file_path = os.path.normpath(os.path.join(self.temp_folder_path, video_title + file_extension))
+            os.rename(temp_file_location, new_file_path)
+
+            return 1, new_file_path
+        except:
+            self.logger.error("read the exception", exc_info=True)
+            return 0, "downloaded video caused error (possibly because of the video title has some bad chars)"
 
 
 
-    def __find_file_extension(self, path_without_extension):
-        """this function exists because youtube-dl does not gives me the path -___-"""
-        for possible_extension in self.possible_extensions:
-            if os.path.isfile(os.path.join(self.temp_folder_path, path_without_extension + possible_extension)):
-                return os.path.join(self.temp_folder_path, path_without_extension + possible_extension)
-        else:
-            return 0
-
-
-    def get_video_types(self):
+    def get_video_formats(self):
+        """returns video formats"""
         self.logger.info("Function name: get_video_types")
-        return str(self.video_formats)
+        str_formats = ""
+        for str_format in self.video_formats:
+            str_formats += str_format + " " 
+        return str_formats
 
 
-    def rm_downloaded_file(self, path):
-        # self.logger.info("Function name: rm_downloaded_file args: {0}".format(path))
-        self.logger.info("Function name: rm_downloaded_file") # cant log some unicode characters
-        if(os.path.normpath(os.path.normcase(os.path.dirname(path))) == os.path.normpath(os.path.normcase(os.path.join(os.path.dirname(os.path.abspath(__file__)), self.temp_folder_path)))):
-            if os.path.isfile(path):
-                os.remove(path)
-        else:
-            self.logger.warning("path is not in the {0}".format(self.temp_folder_path))
 
-        # print(os.path.normcase(os.path.dirname(path)))
-        # print(os.path.normcase(os.path.join(os.path.dirname(os.path.abspath(__file__)), self.temp_folder_path)))
-
-
-    def download(self, link, dl_type = "audio", dl_format = "720p"):
+    def download(self, link, dl_type = "audio", dl_format = "480p"):
         self.logger.info("Function name: download args: {0}, dl_type = {1}, dl_format = {2}".format(link, dl_type, dl_format))
 
-        temp_ydl_options = {}
         if(dl_type == "audio"):
             temp_ydl_options = self.ydl_opts_audio.copy()
         elif(dl_type == "video"):
@@ -114,6 +153,7 @@ class youtube_downloader():
                 self.logger.warning("video format is not supported {0}".format(dl_format))
                 return 0, "video format is not supported"
             
+            # update the format in the template with the selected one
             temp_ydl_options.update({"format":self.video_formats[dl_format]})
 
         else:
@@ -123,27 +163,22 @@ class youtube_downloader():
 
         try:
             with youtube_dl.YoutubeDL(temp_ydl_options) as ydl:
-                
-                # youtube_dl can not override existing video for some reason so I delete it before
-                meta = ydl.extract_info(link, download=False)
-                old_file = self.__find_file_extension(meta["title"])
-                if(old_file):
-                    old_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), old_file)
-                    self.rm_downloaded_file(old_file)
-                    # os.remove(old_file)
+
+                # clear teh temp folder
+                self.__clear_temp_folder()
 
                 # download
-                meta = ydl.extract_info(link, download=True)
-                # ydl.download([link])
+                meta = ydl.extract_info(link, download=True) 
 
-                downloaded_file_path = self.__find_file_extension(meta["title"])
+                # rename the temp video name with videos original title (bad windows path chars are replaced)
+                path_status, info_or_path = self.__rename_downloaded_file(meta["title"])
 
-                if(not downloaded_file_path):
-                    self.logger.error("downloaded video extension is not supported, video title is {0}".format(meta["title"]))
-                    return 0, "downloaded video extension is not supported"
+                # if path_status is 0 return the error
+                if(not path_status):
+                    return 0, info_or_path
 
                 # success
-                return 1, downloaded_file_path
+                return 1, info_or_path
         
         except:
             self.logger.error("download error", exc_info=True)
@@ -162,11 +197,6 @@ if __name__ == "__main__":
     status, path = ytd.download(link, dl_type="audio", dl_format="720p")
 
     print(status, path)
-
-    # delete if exists
-    # if(status):
-    #     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    #     ytd.rm_downloaded_file(path)
 
 
 
