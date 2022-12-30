3df526e33e5228090e68e4bd4a7630b6aec81dfe
cccaaannn <can.kurt.aa@gmail.com>
2022-12-31 00:58:05 +0300

    cli refactor

--- telegram_youtube_downloader/cli.py
+++ telegram_youtube_downloader/cli.py
@@ -5,21 +5,36 @@ import os
 from telegram_bot import TelegramBot
 
 from utils.logger_factory import LoggerFactory
-from utils.utils import Utils
-from utils.statics import Statics
+from utils.api_key_utils import ApiKeyUtils
 
 
 class Cli:
     def __init__(self) -> None:
-        self.bot_key_env_name = Statics.TELEGRAM_BOT_ENVIRONMENT_NAME
-        self.youtube_api_key_env_name = Statics.YOUTUBE_API_ENVIRONMENT_NAME
+        self.bot_key_env_name = ApiKeyUtils.TELEGRAM_BOT_ENVIRONMENT_NAME
+        self.youtube_api_key_env_name = ApiKeyUtils.YOUTUBE_API_ENVIRONMENT_NAME
         self.__logger = LoggerFactory.get_logger(self.__class__.__name__, file_handler=False)
+
+    @staticmethod
+    def __path_type(p):
+        if(os.path.isfile(p)):
+            return p
+        else:
+            raise argparse.ArgumentTypeError("File does not exists")
+
+    @staticmethod
+    def __read_file_lines(file_name):
+        lines = []
+        with open(file_name,'r', encoding='utf-8') as file:
+            content = file.readlines()
+            for line in content:
+                lines.append(line.strip())
+            return lines
 
     def start(self):
         parser = argparse.ArgumentParser(description="Youtube telegram downloader", epilog=f"Uses environment variable <{self.bot_key_env_name}> for the Telegram bot key if no arguments passed")
         mutually_exclusive_group = parser.add_mutually_exclusive_group()
         mutually_exclusive_group.add_argument("-k", "--use_key", dest="use_key", metavar="<key>", type=str, help="Telegram bot and YouTube api key")
-        mutually_exclusive_group.add_argument("-f", "--use_file", dest="use_file", metavar="<path>", type=Utils.path_type, help="File path for Telegram bot and YouTube api key")
+        mutually_exclusive_group.add_argument("-f", "--use_file", dest="use_file", metavar="<path>", type=Cli.__path_type, help="File path for Telegram bot and YouTube api key")
 
         # Get arguments
         args = parser.parse_args()
@@ -30,7 +45,7 @@ class Cli:
             os.environ[self.bot_key_env_name] = api_keys[0]
 
         elif(args.use_file):
-            api_keys = Utils.read_file_lines(args.use_file)
+            api_keys = Cli.__read_file_lines(args.use_file)
             if(len(api_keys) == 2):
                 os.environ[self.youtube_api_key_env_name] = api_keys[1]
             os.environ[self.bot_key_env_name] = api_keys[0]
