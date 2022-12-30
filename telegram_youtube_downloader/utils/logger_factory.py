9fbb32811bd5c338b4c0872e9ce0b7ee18c6862a
cccaaannn <can.kurt.aa@gmail.com>
2022-12-31 00:54:00 +0300

    logger UnicodeEncodeError solved

--- telegram_youtube_downloader/utils/logger_factory.py
+++ telegram_youtube_downloader/utils/logger_factory.py
@@ -1,8 +1,8 @@
 import logging
-import os
 import pathlib
+import os
 
-from utils.utils import Utils
+from utils.config_utils import ConfigUtils
 
 
 class LoggerFactory:
@@ -10,14 +10,15 @@ class LoggerFactory:
     def get_logger(logger_name, file_handler=True):
         """creates logger"""
 
-        cfg = Utils.read_cfg_file()["logger_options"]
+        cfg = ConfigUtils.read_cfg_file()["logger_options"]
         logger = logging.getLogger(logger_name)
         if(not logger.handlers):
+
             logger.setLevel(cfg["log_level"])
-            
+
             # log formatter
             formatter = logging.Formatter(f"[{logger_name}] [%(levelname)s] (%(threadName)s) (%(asctime)s) %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-
+            
             # stream handler
             stream_handler = logging.StreamHandler()
             stream_handler.setFormatter(formatter)
@@ -33,8 +34,8 @@ class LoggerFactory:
                 pathlib.Path(log_path).mkdir(parents=True, exist_ok=True)
 
                 log_file_path = os.path.join(log_path, logger_name + ".log")
-                file_handler = logging.FileHandler(log_file_path)
+                file_handler = logging.FileHandler(log_file_path, encoding='utf-8')
                 file_handler.setFormatter(formatter)
                 logger.addHandler(file_handler)
-            
+
         return logger
