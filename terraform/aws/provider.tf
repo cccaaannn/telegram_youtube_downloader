4722844ec3fb0340f9d52185a5b979d637693de1
cccaaannn <can.kurt.aa@gmail.com>
2025-03-18 21:57:55 +0300

    terraform aws provider

---
+++ terraform/aws/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
