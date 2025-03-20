13eb4be2a0807bbc8f326cdef3b8c2ccfca20005
cccaaannn <can.kurt.aa@gmail.com>
2025-03-21 00:03:02 +0300

    terraform variables

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,7 @@
+variable "_1_telegram_bot_key" {
+  description = "Telegram bot key"
+}
+
+variable "_2_cookie_file_path" {
+  description = "Absolute path of the cookie file (leave empty if not needed)"
+}
